dc981a8437b659742689b41dbd5d6a617f86ef96
Miguel Roa <miguel_roa150@outlook.es>
2022-12-21 10:24:42 -0500

    First Comit

---
+++ src/Footer.jsx
@@ -0,0 +1,22 @@
+import './Footer.css';
+
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <div className="footer-up">
+                <div className="footer-p">
+                    <p>DESCARGO DE RESPONSABILIDAD IMPORTANTE: todo el contenido disponible en nuestro sitio web, en los sitios web hipervinculados,
+                        y en las aplicaciones, foros, blogs, cuentas de redes sociales y otras plataformas asociados ("Sitio") tienen como único
+                        objetivo proporcionarle información general procedente de fuentes externas.</p>
+                </div>
+                <div className="footer-c">
+                    <p>Crypto Stadistics</p>
+                    <p>© 2022</p>
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+export default Footer;
